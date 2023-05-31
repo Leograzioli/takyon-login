b2c1ddd57e8e72dbd8e553e69291ed7796a2c55d
Leograzioli <leograzioli@gmail.com>
2023-05-31 18:01:07 +0200

    add notify

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,22 @@
+import { NavLink } from 'react-router-dom'
 
 export default function Header() {
   return (
     <header className=" bg-primary">
-        <div className="max-w-[1100px] h-[80px] mx-auto flex items-center">
+        <div className="max-w-[1100px] h-[80px] mx-auto flex items-center justify-between">
             <img className="h-[50px]" src="/assets/takyon-logo.jpg" alt="logo takyon" />
+
+            <nav className="text-white font-semibold capitalize ">
+              <ul className='flex gap-x-4'>
+                <li>
+                  <NavLink className={({ isActive }) => isActive ? 'border-b-2 border-white hover:text-gray-500 hover:border-b-gray-500' : 'hover:text-gray-500 hover:border-b-gray-500'} to={'/login'}>Login</NavLink>
+                </li>
+                <li>
+                  <NavLink className={({ isActive }) => isActive ? 'border-b-2 border-white hover:text-gray-500 hover:border-b-gray-500' : 'hover:text-gray-500 hover:border-b-gray-500'} to={'/'}>home</NavLink>
+                </li>
+              </ul>
+            </nav>
+
         </div>
     </header>
   )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,54 +1,71 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login() {
 
-  type User = {
-    email: string,
-  }
-
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [user, setUser] = useState<User | null>(null)
-  const [notify, setNotify] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] =useState('')
 
   const navigate = useNavigate()
+
+  //handle the success notify
+  const succesNotify = () => {
+    toast.success('login successfully, redirecting...'), {
+      position: toast.POSITION.TOP_RIGHT
+    }
+  }
+  
+  //handle the errors notify
+  const errorNotify = (message: string) => {
+    toast.error(message), {
+      position: toast.POSITION.TOP_RIGHT
+    }
+  }
 
   //To handle Login submit. 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     setIsLoading(true)
+    setError('')
 
     axios.post('https://staging-api.takyon.io/auth/login', { email, password })
       .then(resp => {
 
         if (resp.status === 200) {
-          setUser(resp.data);
-          setNotify(true)
           console.log(resp.data);
+          
+          succesNotify()
+          setEmail('')
+          setPassword('')
+
+          setTimeout(() => {
+            navigate('/')
+          }, 2000);
         }
 
-      }).catch(err => {
-        console.log(err.response);
+      }).catch((err) => {
+
+        setError(err.response.data.message);
+        errorNotify(err.response.data.message)
 
       }).finally(() => {
 
         setIsLoading(false)
-
-        setTimeout(() => {
-          navigate('/')
-        }, 1000);
-
       })
 
     new Notification('login')
 
   }
+
   return (
     <div className="bg-secondary">
       <div className="max-w-[1100px] h-[calc(100vh-80px)] mx-auto flex items-center justify-center relative overflow-hidden">
+        <ToastContainer />
 
         {/* login form */}
         <div className="bg-white rounded-md py-5 px-4 border w-4/5 max-w-[400px]">
@@ -58,25 +75,21 @@ export default function Login() {
             <div className='mb-4'>
               <label>
                 <div className='text-xs mb-1 text-t-secondary'>Email</div>
-                <input required type="email" onChange={(e) => setEmail(e.target.value)} className="border rounded-sm bg-light-primary py-1 px-2 focus:outline-gray-400 w-full" />
+                <input required value={email} type="email" onChange={(e) => setEmail(e.target.value)} className={`border ${error.includes('email')? 'border-2 border-red-500' : ''} rounded-sm bg-light-primary py-1 px-2 focus:outline-gray-400 w-full`} />
+                {error.includes('email') && <p className='text-xs text-red-500'>- Email not valid</p>}
               </label>
             </div>
 
             <div className='mb-8'>
               <label>
                 <div className='text-xs mb-1 text-t-secondary'>Password</div>
-                <input required type="password" onChange={(e) => setPassword(e.target.value)} className="border rounded-sm bg-light-primary py-1 px-2 focus:outline-gray-400 w-full" />
+                <input required value={password} type="password" onChange={(e) => setPassword(e.target.value)} className={`border ${error.includes('Password')? 'border-2 border-red-500' : ''} rounded-sm bg-light-primary py-1 px-2 focus:outline-gray-400 w-full`} />
+                {error.includes('Password') && <p className='text-xs text-red-500'>- password not valid</p>}
               </label>
             </div>
 
-            {notify && user &&
-              <div className='mb-2 bg-green-300 w-2/3 mx-auto text-center py-0.5 rounded text-white font-bold'>
-                You are Loged-in
-              </div>
-            }
-
             <div>
-              <button className='bg-primary text-white text-center text-xs font-semibold w-full rounded-sm py-2 uppercase'>{isLoading ? <div>loading</div> : 'Login'}</button>
+              <button className='bg-primary text-white text-center text-xs font-semibold w-full rounded-sm py-2 uppercase'>{isLoading ?  <span>Processing...</span> : 'Login'}</button>
             </div>
 
           </form>
